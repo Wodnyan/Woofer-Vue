5c5266c96cb492ccbb1d1e465b0fc5a943a99324
Albert Gergo <gergoalbert.gta@gmail.com>
2020-10-17 20:45:18 +0300

    Users Records

--- server/db/migrations/20201017125807_initial.js
+++ server/db/migrations/20201017125807_initial.js
@@ -1,5 +1,18 @@
-exports.up = function (knex) {
-  return knex.schema.createTable("users", (table) => {});
+function addDefaultColumns(table) {
+  table.timestamps(false, true);
+  table.datetime("deleted_at");
+}
+
+exports.up = async (knex) => {
+  await knex.schema.createTable("users", (table) => {
+    table.increments().notNullable();
+    table.string("username", 100).notNullable();
+    table.string("password", 200).notNullable();
+    table.string("image_url");
+    addDefaultColumns(table);
+  });
 };
 
-exports.down = function (knex) {};
+exports.down = async (knex) => {
+  await knex.schema.dropTable("users");
+};
