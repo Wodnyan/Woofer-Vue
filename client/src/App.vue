02d610c898878c0c4b23505c46d7ec0162ab24dc
Albert Gergo <gergoalbert.gta@gmail.com>
2020-10-23 19:55:16 +0300

    Added Axios and Circular Button Styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,4 +15,37 @@ html,
 body {
   height: 100%;
 }
+
+.btn.btn-circle,
+.btn.btn-circle.btn-lg,
+.btn-circle.btn-xl {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn.btn-circle {
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  padding: 6px 0;
+  font-size: 12px;
+  line-height: 1.428571429;
+  border-radius: 50%;
+}
+.btn.btn-circle.btn-lg {
+  width: 70px;
+  height: 70px;
+  padding: 10px 16px;
+  font-size: 18px;
+  line-height: 1.33;
+  border-radius: 50%;
+}
+.btn-circle.btn-xl {
+  width: 90px;
+  height: 90px;
+  padding: 10px 16px;
+  font-size: 24px;
+  line-height: 1.33;
+  border-radius: 50%;
+}
 </style>
